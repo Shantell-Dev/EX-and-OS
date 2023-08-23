ac79382e5575ffa623a5d251686318406da798c1
Shantell-Dev <141343347+Shantell-Dev@users.noreply.github.com>
2023-08-23 17:53:47 +0200

    Update game_logic.js
    game JS code complete

--- game_logic.js
+++ game_logic.js
@@ -1,32 +1,64 @@
-let playerText = document.getElementById('playerText')
-let restartBtn = document.getElementById('restartBtn')
-let boxes = Array.from(document.getElementsByClassName('box'))
-//console.log (boxes)
+const playerText = document.getElementById("playerText");
+const restartBtn = document.getElementById("restartBtn");
+const boxes = Array.from(document.getElementsByClassName("box"));
+const winnerIndicator = getComputedStyle(document.body).getPropertyValue("--winning-blocks");
 
-const O_TEXT = "O"
-const X_TEXT = "X"
-let currentPlayer = X_TEXT
-let spaces = Array(9).fill(null)
-//console.log(spaces)
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+const spaces = Array(9).fill(null);
 
 const startGame = () => {
-    console.log("i have started");
-    console.log(boxes);
-    boxes.forEach(box => box.addEventListener('click',boxClicked))
+  boxes.forEach(box => box.addEventListener("click", boxClicked));
 };
-window.addEventListener("load",(event) => {
-  console.log("page is fully loaded");
+
+window.addEventListener("load", () => {
   startGame();
 });
 
 function boxClicked(e) {
-    const id = e.target
-    if(!spaces[id]){
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
-        currentPlayer = currentPlayer === X_TEXT ? O_TEXT: X_TEXT
-    }
-    }
+  const id = e.target.id;
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+    currentPlayer = currentPlayer === X_TEXT ? O_TEXT : X_TEXT;
+    checkForWin();
+    return;
+  }
 }
+
+const winningCombos = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+];
+
+function checkForWin() {
+  for (const combo of winningCombos) {
+    const [a, b, c] = combo;
+    if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
+      updateWinnerUI(combo);
+      return;
+    }
+  }
 }
+
+function updateWinnerUI(winningCombo) {
+  playerText.textContent = `${spaces[winningCombo[0]]} has won!`;
+  winningCombo.forEach(index => boxes[index].style.backgroundColor = winnerIndicator);
+}
+
+restartBtn.addEventListener("click", restart);
+
+function restart() {
+  spaces.fill(null);
+  boxes.forEach(box => {
+    box.innerText = "";
+    box.style.backgroundColor = "";
+  });
+  playerText.textContent = "Xs-and-Os";
+  currentPlayer = X_TEXT;
+}
+
+
 
